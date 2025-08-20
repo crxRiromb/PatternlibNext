bf7a1493d32d0576b0ade141271c8de353d2136d
Richard Romberg <richard.romberg@posteo.de>
2025-08-20 11:03:54 +0200

    feat: temp remove react

--- packages/react-wrapper/dist/button/PlButton.d.ts
+++
@@ -1,5 +0,0 @@
-import { PlButton as PlButtonWebComponent } from '@liebherr2/plnext';
-
-export declare const PlButton: import('@lit/react').ReactWebComponent<PlButtonWebComponent, {
-    onClick: string;
-}>;
